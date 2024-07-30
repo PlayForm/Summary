55b69a28b66f4644c6cb34988f3414bd7baf544d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-30 03:23:43 +0300

    s
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Fn/Summary/Difference.rs
+++ Source/Fn/Summary/Difference.rs
@@ -1,3 +1,5 @@
+use core::fmt::Debug;
+
 /// Calculates the difference between two summaries.
 ///
 /// # Arguments
@@ -23,29 +25,49 @@ pub fn Fn(
 	Option: &Option,
 ) -> Result<String, git2::Error> {
 	let mut Difference = String::new();
+	let mut Options = git2::DiffOptions::new();
+
+	// Options.pathspec(
+	// 	std::ffi::CString::new(
+	// 		std::iter::once("*".to_string())
+	// 			.chain(Option.Omit.iter().map(|Omit| format!("{}", Omit)))
+	// 			.collect::<Vec<String>>()
+	// 			.join("\0"),
+	// 	)
+	// 	.expect("Cannot create CString"),
+	// );
+
+	Options.indent_heuristic(true);
+
+	Options.minimal(true);
+
+	Options.force_text(true);
+
+	Options.ignore_blank_lines(true);
+	Options.ignore_case(true);
+	Options.ignore_filemode(true);
+	Options.ignore_whitespace(true);
+	Options.ignore_whitespace_change(true);
+	Options.ignore_whitespace_eol(true);
+
+	// Options.pathspec("*\n!tsconfig.json");
 
 	Repository
 		.diff_tree_to_tree(
 			Some(&Repository.revparse_single(Start)?.peel_to_commit()?.tree()?),
 			Some(&Repository.revparse_single(End)?.peel_to_commit()?.tree()?),
-			Some(
-				&mut git2::DiffOptions::new().pathspec(
-					std::ffi::CString::new(
-						Option
-							.Omit
-							.iter()
-							.map(|Omit| format!("!{}", Omit))
-							.collect::<Vec<String>>()
-							.join("\0"),
-					)
-					.expect("Cannot create CString"),
-				),
-			),
+			Some(&mut Options),
 		)?
-		.print(git2::DiffFormat::Patch, |_, _, line| {
-			Difference.push_str(std::str::from_utf8(line.content()).unwrap());
-			true
-		})?;
+		.deltas()
+		.for_each(|Delta| {
+			for Omit in &Option.Omit {
+				if Delta.old_file().path().unwrap().display().to_string().contains(Omit)
+					|| Delta.new_file().path().unwrap().display().to_string().contains(Omit)
+				{
+					println!("{:?}", Delta.status());
+				}
+			}
+		});
 
 	Ok(Difference)
 }
